c9eecc407e8151ed6b5e03642849d401d3ee6902
Taylor Guidon <tguidon@gmail.com>
2016-11-29 19:18:40 -0500

    initial commit with podfile setup and installed

---
+++ swift-mvvm/Pods/Target Support Files/Pods-swift-mvvm/Pods-swift-mvvm-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_swift_mvvmVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_swift_mvvmVersionString[];
+
---
+++ swift-mvvm/swift-mvvm/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  swift-mvvm
+//
+//  Created by Taylor Guidon on 11/29/16.
+//  Copyright © 2016 ISL. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var tableView: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
